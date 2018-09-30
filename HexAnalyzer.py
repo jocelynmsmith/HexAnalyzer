b4caaa0cc0230d4d033cd38e2da29e571010c5cd
sawicky <sawickdavid@protonmail.com>
2018-10-01 03:36:51 +1000

    Added result printout

--- HexAnalyzer.py
+++ HexAnalyzer.py
@@ -6,27 +6,33 @@ import os
 
 home = str(Path.home())
 testHome = home + '/Documents/Python Test/'
-for subdir, dirs, files in os.walk(testHome):
+failed = []
+for subdir, dirs, files in os.walk(home):
     for file in files:
         filepath = subdir + os.sep + file
         fileExt = Path(filepath).suffix
-        with open(filepath, 'rb') as f:
-                testHex = []
-                fileHex = f.read(32)
-                print('Filename: '+ file)
-                for sig in signatures:
-                    if fileExt == sig['ext']:
-                        print('File extension matches pattern in db: '+ fileExt)
-                        expectedHex = str(json.dumps(sig['hex'])[1:-1])
-                        print('Expected hex: ' + expectedHex)
-                        sigLen = len(json.dumps(sig['hex']))
-                        gotHex = str(fileHex.hex()[0:sigLen-2]).upper()
-                        print('Got hex: ' + gotHex)
-                        if (gotHex == expectedHex):
-                            print('hexes match')
-                        else :
-                            print('File extension does not match database - Possibly edited')
+        try:
+            with open(filepath, 'rb') as f:
+                    fileHex = f.read(32)
+                    print('Filename: '+ file)
+                    for sig in signatures:
+                        if fileExt == sig['ext']:
+                            print('File extension matches pattern in db: '+ fileExt)
+                            expectedHex = str(json.dumps(sig['hex'])[1:-1])
+                            print('Expected hex: ' + expectedHex)
+                            sigLen = len(json.dumps(sig['hex']))
+                            gotHex = str(fileHex.hex()[0:sigLen-2]).upper()
+                            print('Got hex: ' + gotHex)
+                            if (gotHex == expectedHex):
+                                print('hexes match')
+                            else :
+                                print('File extension does not match database - Possibly edited')
+                                failed.append(filepath)
+        except:
+            pass
 
-                print("\n")
+print ('Failed files: ')
+for s in failed:
+    print(s)
 
 
